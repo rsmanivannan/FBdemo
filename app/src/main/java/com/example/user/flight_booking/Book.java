f93d8815bcc3c22321bbe4f8efcc1ea628ec60c3
rsmanivannan <rs_manivannan@yahoo.co.in>
2019-06-12 11:32:10 +0000

    mod18 demo

--- app/src/main/java/com/example/user/flight_booking/Book.java
+++ app/src/main/java/com/example/user/flight_booking/Book.java
@@ -34,7 +34,7 @@ public class Book extends AppCompatActivity {
         spinner2 = (Spinner) findViewById(R.id.spinnerDest);
 
         // initiate the date picker and a button
-        //code changed for the demo
+        //modified code for demo
 
         dates = (EditText) findViewById(R.id.dateStart);
         // perform click event on edit text
